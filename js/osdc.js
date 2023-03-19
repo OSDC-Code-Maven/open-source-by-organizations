dde56ab398addd8dad393d3b7480798ba5517504
Gabor Szabo <gabor@szabgab.com>
2023-03-19 21:52:29 +0200

    make tables sortable #15

--- js/osdc.js
+++ js/osdc.js
@@ -4,4 +4,39 @@ $(document).ready(function(){
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
   });
+
+  // Sorting from: https://orangeable.com/javascript/jquery-table-sorting
+  $(document).on("click", "table thead tr th:not(.no-sort)", function() {
+  	var table = $(this).parents("table");
+  	var rows = $(this).parents("table").find("tbody tr").toArray().sort(TableComparer($(this).index()));
+  	var dir = ($(this).hasClass("sort-asc")) ? "desc" : "asc";
+
+  	if (dir == "desc") {
+  		rows = rows.reverse();
+  	}
+
+  	for (var i = 0; i < rows.length; i++) {
+  		table.append(rows[i]);
+  	}
+
+  	table.find("thead tr th").removeClass("sort-asc").removeClass("sort-desc");
+  	$(this).removeClass("sort-asc").removeClass("sort-desc") .addClass("sort-" + dir);
+  });
+
+  function TableComparer(index) {
+  	return function(a, b) {
+  		var val_a = TableCellValue(a, index);
+  		var val_b = TableCellValue(b, index);
+  		var result = ($.isNumeric(val_a) && $.isNumeric(val_b)) ? val_a - val_b : val_a.toString().localeCompare(val_b);
+
+  		return result;
+  	}
+  }
+
+  function TableCellValue(row, index) {
+  	return $(row).children("td").eq(index).text();
+  }
+  // end table sort
+
+
 });
