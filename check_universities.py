5dfce0bd72c2b0b9b9a237e0f8394f4ee57930af
Gabor Szabo <gabor@szabgab.com>
2023-06-28 10:09:22 +0300

    update the checking of universities

--- check_universities.py
+++ check_universities.py
@@ -9,9 +9,17 @@ import yaml
 # then this path should be valid:
 filename = os.path.join('..', 'university-domains-list', 'world_universities_and_domains.json')
 
+# already checked and could not find github organization
+skip = set([
+    'abcnash.edu',
+    'selmauniversity.edu', # no such domain
+    'swcc.edu',
+    'vuu.edu',
+])
+
 def get_all_the_domains_we_already_have():
     organizations = set([name[0:-5] for name in os.listdir(os.path.join('data', 'organisations'))])
-    print(sorted(organizations))
+    #print(sorted(organizations))
     for github_file in os.listdir(os.path.join('data', 'github')):
         #print(github_file)
         with open(os.path.join('data', 'github', github_file)) as fh:
@@ -26,15 +34,17 @@ def get_all_the_domains_we_already_have():
 
 
 def main():
-    get_all_the_domains_we_already_have()
+    organizations = get_all_the_domains_we_already_have()
 
     # go over the domain in the other list and print the ones we are missing
-    #with open(filename) as fh:
-    #    universities = json.load(fh)
+    with open(filename) as fh:
+        universities = json.load(fh)
 
-    #    for univ in universities:
-    #        for domain in univ['domains']:
-    #            if not os.path.exists('data/organisations/{domain}.yaml'):
-    #                print(domain)
+        for univ in universities:
+            for domain in univ['domains']:
+                if domain in skip:
+                    continue
+                if not domain in organizations:
+                    print(domain)
 
 main()
